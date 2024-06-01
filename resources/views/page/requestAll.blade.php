c257b01694b077763f086172b4100ce9fc9b5a5d
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2024-06-01 11:37:15 +0700

    Update requestAll.blade.php

--- resources/views/page/requestAll.blade.php
+++ resources/views/page/requestAll.blade.php
@@ -10,72 +10,7 @@
             </div>
 
             <div class="sm:rounded-lg p-4 row gap-3 justify-center">
-                @foreach ($requests as $index => $req)
-                    <div class="max-w-sm p-6 border-sm  border-gray-200 rounded-lg shadow {{ $req->status === '1' ? 'bg-green-200' : ($req->status === '2' ? 'bg-red-200' : 'bg-white') }}">
-                        @if ($req->type === 'course')
-                            <a href="#">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">คำขอเพิ่มหลักสูตร</h5>
-                            </a>
-                        @else
-                            <a href="#">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">คำขออื่นๆ</h5>
-                            </a>
-                        @endif
-                        <div class="grid grid-cols-2">
-                            <p class="font-normal text-gray-700">จาก: {{ $req->getUser->name }}</p>
-                            @if ($req->target === '-')
-                                <p class="font-normal text-gray-700">ให้: {{ $req->target }}</p>
-                            @else
-                                <p class="font-normal text-gray-700">ให้: {{ $req->getTarget->name }}</p>
-                            @endif
-                            <p class="font-normal text-gray-700">เมื่อ: {{ Carbon\Carbon::parse($req->created_at)->setTimezone('Asia/Bangkok')->locale('th')->thaidate('j M Y') }}</p>
-                        </div>
-                        <div class="mb-3">
-                            @if ($req->type === 'course')
-                                <p class="font-normal text-gray-700">หลักสูตร:</p>
-                                @foreach ((App\Models\course::whereIn('id', json_decode($req->content))->pluck('code', 'title') ?? []) as $title => $code)
-                                    <p class="font-normal text-gray-700 ms-4">- {{ $code }} :: {{ $title }}</p>
-                                @endforeach
-                            @else
-                                <p class="font-normal text-gray-700">ข้อความ:</p>
-                                <p class="font-normal text-gray-700 ms-4">{{ $req->content }}</p>
-                            @endif
-                        </div>
 
-                        @if ($req->status === '0')
-                            @hasanyrole('staff|admin')
-                                <a href="#" data-alert-id="{{ $req->id }}" class="finishBtn inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
-                                    สำเร็จ
-                                </a>
-                                <a href="#" data-alert-id="{{ $req->id }}" class="failBtn inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800">
-                                    ไม่สำเร็จ
-                                </a>
-                            @else
-                                <p class="w-100 justify-center inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-500 rounded-lg">
-                                    กำลังดำเนินการ
-                                </p>
-                            @endhasanyrole
-                        @elseif ($req->status === '1')
-                            <button data-tooltip-target="tooltip-default" type="button" class="w-100 justify-center inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-500 rounded-lg">
-                                ดำเนินการสำเร็จ
-                            </button>
-                            <div id="tooltip-default" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                                ผู้ดำเนินการ: {{ optional($req->getFinish)->name }} <br>
-                                วันที่ดำเนินการ: {{ Carbon\Carbon::parse($req->created_at)->setTimezone('Asia/Bangkok')->locale('th')->thaidate('j M Y') }}
-                                <div class="tooltip-arrow" data-popper-arrow></div>
-                            </div>
-                        @else
-                            <button data-tooltip-target="tooltip-default" type="button" class="w-100 justify-center inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg">
-                                ดำเนินการไม่สำเร็จ
-                            </button>
-                            <div id="tooltip-default" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                                ผู้ดำเนินการ: {{ optional($req->getFinish)->name }} <br>
-                                วันที่ดำเนินการ: {{ Carbon\Carbon::parse($req->created_at)->setTimezone('Asia/Bangkok')->locale('th')->thaidate('j M Y') }}
-                                <div class="tooltip-arrow" data-popper-arrow></div>
-                            </div>
-                        @endif
-                    </div>
-                @endforeach
             </div>
         </div>
     </div>

ad942ec0f7086580950ba501cf522d20b6c2e779
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-01-30 17:20:38 +0700

    Update course-detail.blade.php

--- resources/views/page/courses/course-detail.blade.php
+++ resources/views/page/courses/course-detail.blade.php
@@ -26,11 +26,11 @@
                             <div class="ps-4">
                                 <p class="mb-3" style="text-indent: 1.5em">{!! $lesson->desc !!}</p>
                                 @php
-                                    $strSubless = $lesson->sub_lessons;
-                                    $sublesson = json_decode($strSubless, true);
+                                    dd($sls->type);
                                 @endphp
                                 @php
-                                    dd($sls->type);
+                                    $strSubless = $lesson->sub_lessons;
+                                    $sublesson = json_decode($strSubless, true);
                                 @endphp
                                 @if (!is_null($sublesson))
                                     @foreach ($sublesson as $index => $sls)

53eb66a65f943e9e59130e8fee63894f1503c5f7
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-08-30 14:46:58 +0700

    addFilter

--- app/Livewire/LearningReport.php
+++ app/Livewire/LearningReport.php
@@ -11,12 +11,16 @@ use Livewire\Component;
 
 class LearningReport extends Component
 {
-    public $user_courses;
+    public $users;
+    public $courses;
+    public $filter_user;
+    public $filter_course;
     public $course_progreses;
     public $formData = [];
     public function mount()
     {
-        // $users = User::orderBy('created_at', 'desc')->get(['id']);
+        $this->users = User::orderBy('created_at', 'desc')->get(['id', 'name']);
+        $this->courses = course::orderBy('created_at', 'desc')->get(['id', 'title']);
 
         $this->course_progreses = progress::select(
             'user_id',
@@ -25,6 +29,32 @@ class LearningReport extends Component
             DB::raw('MAX(created_at) as last_learned_at')
         )->groupBy(['user_id', 'course_id'])->orderBy('last_learned_at', 'desc')->get();
     }
+
+    public function filterLearning() {
+        // $this->resetPage();
+        $query = progress::select(
+            'user_id',
+            'course_id',
+            DB::raw('COUNT(*) as learned_lesson'),
+            DB::raw('MAX(created_at) as last_learned_at')
+        );
+
+        if ($this->filter_user) {
+            $query->where('user_id', $this->filter_user);
+        }
+
+        if ($this->filter_course) {
+            $query->where('course_id', $this->filter_course);
+        }
+
+        $this->course_progreses = $query->groupBy(['user_id', 'course_id'])->orderBy('last_learned_at', 'desc')->get();
+    }
+
+    // public function updated($propertyName)
+    // {
+    //     // เมื่อ filter ถูกเปลี่ยน ให้กลับไปหน้าแรกเสมอ
+    //     $this->resetPage();
+    // }
 
     public function render()
     {
--- resources/views/livewire/learning-report.blade.php
+++ resources/views/livewire/learning-report.blade.php
@@ -1,6 +1,33 @@
 <div class="bg-white p-4 rounded shadow-sm min-w-full">
-    <div class="flex flex-wrap justify-between mb-3">
+    <div class="flex flex-wrap justify-between mb-3 gap-x-3">
         <p class="text-2xl font-bold"><i class="bi bi-backpack"></i>{{ __('messages.All Course') }}</p>
+        <form wire:submit.prevent="filterLearning">
+            <div class="flex gap-2">
+                <select id="small" wire:model="filter_user" aria-label="Filter quiz"
+                    class="w-60 block p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500">
+                    <option value="" selected>ผู้ใช้ทั้งหมด</option>
+                    @foreach ($users as $user)
+                        <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    @endforeach
+                </select>
+                <select id="small" wire:model="filter_course" aria-label="Filter quiz"
+                    class="w-60 block p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500">
+                    <option value="" selected>หลักสูตรทั้งหมด</option>
+                    @foreach ($courses as $course)
+                        <option value="{{ $course->id }}">{{ $course->title }}</option>
+                    @endforeach
+                </select>
+                <button type="submit"
+                        class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                    <span class="sr-only">Search</span>
+                </button>
+            </div>
+        </form>
     </div>
     <div class="overflow-x-auto">
         <table class="table table-hover" id="course-datatable">
@@ -31,7 +58,7 @@
                             <td data-toggle="tooltip" data-placement="top" title="adwadawdawdaw">
                                 {{ Str::limit(optional($course_progres->course)->title, 60) }}</td>
                             <td class="text-center">
-                                {{ $prog_finish}} / {{$less_all}} ({{ $prog_avg }}%)
+                                {{ $prog_finish }} / {{ $less_all }} ({{ $prog_avg }}%)
                                 {{-- <div class="progress" role="progressbar" aria-label="Example with label"
                                     aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
                                     <div class="progress-bar bg-success" style="width: {{ $prog_avg }}%">
@@ -39,7 +66,7 @@
                                 </div> --}}
                             </td>
                             <td class="text-end">
-                                {{ $course_progres->last_learned_at ? Carbon\Carbon::parse($course_progres->last_learned_at)->thaidate('j M Y') : "-" }}
+                                {{ $course_progres->last_learned_at ? Carbon\Carbon::parse($course_progres->last_learned_at)->thaidate('j M Y') : '-' }}
                             </td>
                         </tr>
                     @endforeach

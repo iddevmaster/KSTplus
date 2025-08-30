029fa38f32029997359730e88e0c236f8137ac56
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-08-30 14:53:31 +0700

    Update test-report.blade.php

--- resources/views/report/test-report.blade.php
+++ resources/views/report/test-report.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="text-center mt-5 px-2">
-            <p class="fs-1 fw-bold">Test Report</p>
+            <p class="fs-1 fw-bold">รายงานการทำแบบทดสอบ</p>
             {{-- course Content --}}
             <div class="mt-4">
                 <livewire:test-report />

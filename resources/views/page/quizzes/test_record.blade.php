5d20e7ddce36f0ca4463b611db59304177f0a32e
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-09-15 13:38:17 +0700

    Update test_record.blade.php

--- resources/views/page/quizzes/test_record.blade.php
+++ resources/views/page/quizzes/test_record.blade.php
@@ -70,7 +70,7 @@
                                     </td>
                                     <td class="px-6 py-4">
                                         <a href="{{ route('test.history.show', ['testid' => $test->id]) }}" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center">
-                                            <i class="bi bi-list"></i>
+                                            รายละเอียด
                                         </a>
                                     </td>
                                 </tr>
